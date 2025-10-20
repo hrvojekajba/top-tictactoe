cfead7ad82994bd86a6fc411514ce0aaa271c205
hrvojekajba <172497559+hrvojekajba@users.noreply.github.com>
2025-10-20 17:54:21 +0200

    Fixed up cell rendering

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
+let gameFinished = false;
+
 const gameboardDiv = document.querySelector(".gameboard-div");
-const renderButton = document.getElementById("render-button");
 
 const gameboard = (() => {
     let cells = ["", "", "", "", "", "", "", "", ""];
 
-    const renderCells = () => {
+    function renderCells() {
         gameboardDiv.innerHTML = ``;
         cells.forEach((cell, index) => {
             let cellDiv = document.createElement("div");
             cellDiv.className = "cell-div";
-            cellDiv.id = index;
+            cellDiv.dataset.id = index;
             cellDiv.innerHTML = cell;
             gameboardDiv.appendChild(cellDiv);
         });
-    };
+    }
 
-    return { cells, renderCells };
+    function handleClick(targetDiv, sign) {
+        cells[targetDiv.dataset.id] = sign;
+        renderCells();
+    }
+
+    return { cells, renderCells, handleClick };
 })();
 
-renderButton.addEventListener("click", () => {
-    gameboard.renderCells();
+gameboard.renderCells();
+
+gameboardDiv.addEventListener("click", (e) => {
+    e.preventDefault();
+    gameboard.handleClick(e.target, "x");
 });
