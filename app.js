772b54cf4bea8b5cfdf39aa5a455c5d219b5ffce
hrvojekajba <172497559+hrvojekajba@users.noreply.github.com>
2025-11-26 17:54:59 +0100

    Changed code formatting

--- app.js
+++ app.js
@@ -3,30 +3,30 @@ let gameFinished = false;
 const gameboardDiv = document.querySelector(".gameboard-div");
 
 const gameboard = (() => {
-    let cells = ["", "", "", "", "", "", "", "", ""];
+  let cells = ["", "", "", "", "", "", "", "", ""];
 
-    function renderCells() {
-        gameboardDiv.innerHTML = ``;
-        cells.forEach((cell, index) => {
-            let cellDiv = document.createElement("div");
-            cellDiv.className = "cell-div";
-            cellDiv.dataset.id = index;
-            cellDiv.innerHTML = cell;
-            gameboardDiv.appendChild(cellDiv);
-        });
-    }
+  function renderCells() {
+    gameboardDiv.innerHTML = ``;
+    cells.forEach((cell, index) => {
+      let cellDiv = document.createElement("div");
+      cellDiv.className = "cell-div";
+      cellDiv.dataset.id = index;
+      cellDiv.innerHTML = cell;
+      gameboardDiv.appendChild(cellDiv);
+    });
+  }
 
-    function handleClick(targetDiv, sign) {
-        cells[targetDiv.dataset.id] = sign;
-        renderCells();
-    }
+  function handleClick(targetDiv, sign) {
+    cells[targetDiv.dataset.id] = sign;
+    renderCells();
+  }
 
-    return { cells, renderCells, handleClick };
+  return { cells, renderCells, handleClick };
 })();
 
 gameboard.renderCells();
 
 gameboardDiv.addEventListener("click", (e) => {
-    e.preventDefault();
-    gameboard.handleClick(e.target, "x");
+  e.preventDefault();
+  gameboard.handleClick(e.target, "x");
 });
